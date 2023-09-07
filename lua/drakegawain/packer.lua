eff0fac511d9ecde3e163d551c005520b5b63465
drakegawain <thales.souzalopes@gmail.com>
2023-09-06 21:48:15 -0300

    add configs

--- lua/drakegawain/packer.lua
+++ lua/drakegawain/packer.lua
@@ -18,10 +18,34 @@ return require('packer').startup(function(use)
 		vim.cmd('colorscheme rose-pine')
 	end
   })
+  use({'bluz71/vim-moonfly-colors', as ='moonfly'})
   use('nvim-treesitter/nvim-treesitter', {run= ':TSUpdate'})
-  use('nvim-treesitter/nvim-treesitter')
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+
+
+  use {
+      'VonHeikemen/lsp-zero.nvim',
+      requires = {
+          -- LSP Support
+          {'neovim/nvim-lspconfig'},
+          {'williamboman/mason.nvim'},
+          {'williamboman/mason-lspconfig.nvim'},
+
+          -- Autocompletion
+          {'hrsh7th/nvim-cmp'},
+          {'hrsh7th/cmp-buffer'},
+          {'hrsh7th/cmp-path'},
+          {'hrsh7th/cmp-nvim-lsp'},
+          {'hrsh7th/cmp-nvim-lua'},
+          {'saadparwaiz1/cmp_luasnip'},
+
+          -- Snippets
+          {'L3MON4D3/LuaSnip'},
+          {'rafamadriz/friendly-snippets'},
+      }
+   }
+
   end)
 
